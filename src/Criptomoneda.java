dd7c21f9c149b75777deb5a45de10a49e27c6c3a
roberto3554 <roberto35@correo.ugr.es>
2024-11-06 10:48:11 +0000

    Primera versión del archivo Criptomoneda.java

---
+++ src/Criptomoneda.java
@@ -0,0 +1,9 @@
+public class Criptomoneda{
+	private String nombre;
+	private dobule valorDeToken;
+
+	public Criptomoneda(String nomvre, double valorDeToken){
+		this.nombre = nombre;
+		this.valorDeToken = valorDeToken;
+	}
+}
